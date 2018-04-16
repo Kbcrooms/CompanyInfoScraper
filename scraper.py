675f7022b782806bf3ac2186bcef09c1add8fdef
Khristan Crooms <kbcrooms@localhost.localdomain>
2018-04-16 16:29:31 -0400

    Email Address scraper working

--- scraper.py
+++ scraper.py
@@ -1,5 +1,25 @@
-import requests
-url = "https://google.com/search?q=pokemon"
-html = requests.get(url)
+import requests, re, html2text
+from bs4 import BeautifulSoup
 
-print(html.content)
+website = "www.sysplus.com/"
+foundEmails = []
+searchQuery = re.search('(?:www.)?([a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:\.[a-zA-Z]+)+)',website,re.M|re.I)
+searchEmail = "info%40"+searchQuery.group(1)
+for i in range(0,5):
+
+    searchPage = '&start='+ str(i*10)
+    searchURL = "https://google.com/search?q=" + searchEmail + searchPage
+    html = requests.get(searchURL)
+    text_maker = html2text.HTML2Text()
+    text_maker.ignore_emphasis = True
+
+    htmlText = text_maker.handle(BeautifulSoup(html.content,"html5lib").prettify())
+    foundEmails += re.findall(r'([\-\w\d.]+\s*\@\s*'+re.escape(searchQuery.group(1))+')',htmlText)
+    #print("CONTENT START")
+    #print(htmlText)
+    print (htmlText)
+    #print("CONTENT END")
+    #print('[-\w\d.]+\@'+re.escape(searchQuery.group(1)))
+for email in foundEmails:
+    print("".join(email.split()))
+#print(foundEmails)
